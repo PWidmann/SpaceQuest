173098c43014ab9e466110149af067e05bec9618
PWidmann <60736526+PWidmann@users.noreply.github.com>
2022-06-26 21:40:37 +0200

    Planet Generator started
    Sebastian Lague Tutorial up to video 3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,14 +8,47 @@ public class PlayerController : MonoBehaviour
     private Animator animator;
     private CharacterController controller;
 
+    Transform cameraT;
 
     // Movement
     private Vector2 inputDir;
     private Vector3 velocity;
+    private float velocityY;
+    private float turnSmoothVelocity;
+    float speedSmoothVelocity;
+    private float speedSmoothTime = 0.1f;
+    public float turnSmoothTime = 0.1f;
+    private float runSpeed = 6f;
+    float currentSpeed;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
         controller = GetComponent<CharacterController>();
+        cameraT = Camera.main.transform;
+    }
+
+    private void Update()
+    {
+        //Movement
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        inputDir = input.normalized;
+        Move(inputDir);
+    }
+
+    private void Move(Vector2 inputDir)
+    {
+        if (inputDir != Vector2.zero)
+        {
+            float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+        }
+
+        float targetSpeed = runSpeed * inputDir.magnitude;
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+
+        velocity = transform.forward * currentSpeed + Vector3.up * velocityY;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
